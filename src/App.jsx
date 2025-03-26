b62232e74280025d4a0a49aef5cf9f745b18f9d0
KeerthiEllanki-BilliMD <keerthi@billimd.com>
2025-03-26 14:21:24 -0400

    routerchange

--- src/App.jsx
+++ src/App.jsx
@@ -17,15 +17,15 @@ function App() {
       <Navbar />
       <main className="w-full pt-24 px-6 flex-grow">
         <Routes>
-          <Route path="/" element={<About />} />
-          <Route path="/education" element={<Education />} />
-          <Route path="/experience" element={<Experience />} />
+          <Route path="" element={<About />} />
+          <Route path="education" element={<Education />} />
+          <Route path="experience" element={<Experience />} />
           {/* <Route path="/projects" element={<Projects />} /> */}
-          <Route path="/skills" element={<Skills />} />
+          <Route path="skills" element={<Skills />} />
           {/* <Route path="/volunteer" element={<Volunteer />} /> */}
-          <Route path="/achievements" element={<Achievements />} />
+          <Route path="achievements" element={<Achievements />} />
           {/* <Route path="/contact" element={<Contact />} /> */}
-          <Route path="*" element={<Navigate to="/" />} />
+          {/* <Route path="*" element={<Navigate to="/" />} /> */}
         </Routes>
       </main>
       <Footer />
